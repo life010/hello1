08d88cf1e49e2091016a559f628e0b35709584e7
unknown <life010@naver.com>
2014-11-21 11:32:25 +0900

    gugudan class

--- gugudan.java
+++ gugudan.java
@@ -5,8 +5,9 @@ public class gugudan {
 for(int i=2; i<=9; i++) {
 	for(int j=1; j<=9; j++)
 		{
-		System.out.print(i+"x"+j+"="+i*j);
+		System.out.print(i+"x"+j+"="+i*j+"\t");
 		}
+	System.out.println("");
 	}
 	}
 }
